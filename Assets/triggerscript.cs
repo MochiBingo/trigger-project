4b205a2c78e3502b528553fa07d66c3adb195008
MochiBingo <mochibingoshep@gmail.com>
2024-10-31 18:07:17 -0300

    finished

--- Assets/triggerscript.cs
+++ Assets/triggerscript.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class triggerscript : MonoBehaviour
 {
     public GameObject plane;
+    public AudioClip clip;
     void OnTriggerEnter(Collider other)
     {
         plane.SetActive(true);
+        AudioSource audio= GetComponent<AudioSource>();
+        audio.Play();
+        audio.clip = clip;
+        audio.Play();
     }
 }
